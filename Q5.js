d595fd1eb8792d107f9d20a924432a5ebee01601
NoreenZubair231 <160269643+NoreenZubair231@users.noreply.github.com>
2024-02-17 02:16:03 +0500

    Assignment 5

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+//Q4. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Albert Einstein,";
+let message = "Imagination is more important than knowledge.For knowledge is limited to all we know and understand,while imagination embraces the entire world,and all there ever will be to know and understand";
+console.log(`${famous_person} "${message}"`);
---
+++ Q5.ts
@@ -0,0 +1,7 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string="Albert Einstein,";
+
+let message:string = "Imagination is more important than knowledge.For knowledge is limited to all we know and understand,while imagination embraces the entire world,and all there ever will be to know and understand";
+
+console.log(`${famous_person} "${message}"`);
